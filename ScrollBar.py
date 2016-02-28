ff9283f17d0e4d3abd5a547b4ee84c668d2dd471
Nadeem-Doc <doctorwm@uni.coventry.ac.uk>
2016-02-28 11:14:04 +0000

    Update ScrollBar.py

--- ScrollBar.py
+++ ScrollBar.py
@@ -1,26 +1,25 @@
 from Tkinter import *
 
-
 root = Tk()
+#fame created to group listbox and scroll widgets
+frame1 = Frame(root)
+frame1.pack(side = BOTTOM)
+
+label = Label(root, text = "***\\\TEXT///***")
+label.pack(side = LEFT)
 
 
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill= Y)
 
-scrollbar = Scrollbar(root)
-scrollbar.pack(side = RIGHT, fill = Y)
+listbox = Listbox(frame1, yscrollcommand=scrollbar.set)
 
-
-mylist = Listbox(root, yscrollcommand = scrollbar.set )
-
-
-#Prints string to the box within the List
-for line in range(25):
-   mylist.insert(END, "This is line number " + str(line))
+#can be deleted. used to show the scroll function 
+for i in range(1000):
+    listbox.insert(END, str(i))
 
 
-
-mylist.pack(side = LEFT, fill = BOTH )
-scrollbar.config( command = mylist.yview )
-
-
+listbox.pack(side=LEFT, fill= BOTH)
+scrollbar.config(command=listbox.yview)
 
 mainloop()
